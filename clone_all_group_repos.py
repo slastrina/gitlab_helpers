4949ce73bacee238065745159221e066f4893054
Samuel <50973822+slastrina@users.noreply.github.com>
2020-01-15 12:01:33 +1100

    support for multiple group names

--- clone_all_group_repos.py
+++ clone_all_group_repos.py
@@ -6,16 +6,27 @@ from git import Repo
 
 gitlab_server = os.getenv('gitlab_server_addr')
 gitlab_token = os.getenv('gitlab_token')
-gitlab_group_name = 'eis'
+gitlab_group_names = ['eis']
 
-destination = os.path.join(str(Path.home()), 'downloads/repos/')
-
-os.makedirs(destination, exist_ok=True)
+repo_root_dir = os.path.join(str(Path.home()), 'downloads', 'repos')
 
 gl = gitlab.Gitlab(gitlab_server, private_token=gitlab_token, ssl_verify=False)
 
-group = gl.groups.get(gitlab_group_name)
-projects = group.projects.list(all=True)
-for project in projects:
-    print('Cloning:', project.name, project.http_url_to_repo)
-    Repo.clone_from(project.http_url_to_repo, os.path.join(destination, project.name))
+for group_name in gitlab_group_names:
+    destination = os.path.join(repo_root_dir, group_name)
+    os.makedirs(destination, exist_ok=True)
+
+    print(f"Retrieving list of {group_name} repository's")
+    group = gl.groups.get(group_name)
+    projects = group.projects.list(all=True)
+
+    for project in projects:
+        clone_path = os.path.join(destination, project.name)
+
+        if os.path.exists(clone_path):
+            for remote in Repo(clone_path).remotes:
+                print(f'Fetching {group_name}:', project.name, remote.name)
+                remote.fetch()
+        else:
+            print(f'Cloning {group_name}:', project.name, project.http_url_to_repo)
+            Repo.clone_from(project.http_url_to_repo, os.path.join(destination, clone_path))
