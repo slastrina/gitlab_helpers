efe4079a6c1f9acf4efa6d37a2d06312ed0d0e1d
Samuel <50973822+slastrina@users.noreply.github.com>
2020-01-15 11:48:31 +1100

    added clone repo capability

--- clone_all_group_repos.py
+++ clone_all_group_repos.py
@@ -1,14 +1,21 @@
 import os
+from pathlib import Path
 
 import gitlab
+from git import Repo
 
 gitlab_server = os.getenv('gitlab_server_addr')
 gitlab_token = os.getenv('gitlab_token')
 gitlab_group_name = 'eis'
+
+destination = os.path.join(str(Path.home()), 'downloads/repos/')
+
+os.makedirs(destination, exist_ok=True)
 
 gl = gitlab.Gitlab(gitlab_server, private_token=gitlab_token, ssl_verify=False)
 
 group = gl.groups.get(gitlab_group_name)
 projects = group.projects.list(all=True)
 for project in projects:
-    print(project.name, project)
+    print('Cloning:', project.name, project.http_url_to_repo)
+    Repo.clone_from(project.http_url_to_repo, os.path.join(destination, project.name))
